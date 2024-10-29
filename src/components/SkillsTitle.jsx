1f630e518cca96572588657d958ba7c08cbbde6e
Bastien Fournier <bastien.fournier06@gmail.com>
2024-10-29 17:03:11 +0100

    fix picture

--- src/components/SkillsTitle.jsx
+++ src/components/SkillsTitle.jsx
@@ -6,10 +6,11 @@ export default function SkillsTitle() {
       <div className="ml-0 lg:ml-40">
         <figure className="flex flex-col items-center">
           <img
-            className="w-48 h-48 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-96 lg:h-96 object-cover shadow-lg rounded-lg"
+            className="w-48 h-48 sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-96 lg:h-96 object-contain "
             src={GroupePicture}
             alt="Groupe"
           />
+
           <figcaption className="text-center mt-2 text-sm text-gray-600">
             Hackathon à la Wild Code School
           </figcaption>
